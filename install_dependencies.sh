abb76b6d37dfc6083395f31ef3b8a5770a6318f4
InverTome <104219631+invertome@users.noreply.github.com>
2022-12-23 17:13:47 -0700

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# update conda
-conda update -n base -c defaults conda
 
 # create a new conda environment
-conda create -n MLtests python=3.7
+conda create -n MLtests python=3.7 -y
 
 # activate the new environment
 conda activate MLtests
@@ -15,13 +13,13 @@ conda config --add channels bioconda
 conda config --add channels conda-forge
 
 # install the dependencies
-conda install -c anaconda argparse
-conda install -c anaconda ipython
-conda install -c anaconda numpy
-conda install -c anaconda scikit-learn
-conda install -c anaconda scipy
-conda install -c bioconda biopython
-conda install -c anaconda pandas
-conda install -c anaconda seaborn
-conda install -c anaconda matplotlib
-conda install -c conda-forge tensorflow
+conda install -c anaconda argparse -y
+conda install -c anaconda ipython -y
+conda install -c anaconda numpy -y
+conda install -c anaconda scikit-learn -y
+conda install -c anaconda scipy -y
+conda install -c bioconda biopython -y
+conda install -c anaconda pandas -y
+conda install -c anaconda seaborn -y
+conda install -c anaconda matplotlib -y
+conda install -c conda-forge tensorflow -y
